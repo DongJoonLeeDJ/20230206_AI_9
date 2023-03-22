50ff796bf6b0553ff22f89ca7cd5d33405f14ece
Xenoint <ldjgood12@gmail.com>
2023-03-22 14:43:16 +0900

    c 업데이트

--- C/20230223/AdvancedCLang/AdvancedCLang/패스워드 문제.c
+++ C/20230223/AdvancedCLang/AdvancedCLang/패스워드 문제.c
@@ -2,20 +2,27 @@
 #include <ctype.h>
 #include <string.h>
 
-int password_check2(char* password) {
+int password_check2(char* password) 
+{
     int digitIs = 0, alphaIs = 0, specialIs = 0;
     char allowed_special[] = { '!', '_' };
 
-    for (int i = 0; i < strlen(password); i++) {
-        if (isdigit(password[i])) {
+    for (int i = 0; i < strlen(password); i++) 
+	{
+        if (isdigit(password[i])) 
+		{
             digitIs = 1;
         }
-        else if (isalpha(password[i])) {
+        else if (isalpha(password[i])) 
+		{
             alphaIs = 1;
         }
-        else {
-            for (int j = 0; j < sizeof(allowed_special); j++) {
-                if (password[i] == allowed_special[j]) {
+        else 
+		{
+            for (int j = 0; j < sizeof(allowed_special); j++) 
+			{
+                if (password[i] == allowed_special[j]) 
+				{
                     specialIs = 1;
                     break;
                 }
@@ -28,13 +35,16 @@ int password_check2(char* password) {
         return 0;
 }
 
-int main() {
+int main() 
+{
     char* keyInStr[200] = { 0 };
 
-    while (1) {
+    while (1) 
+	{
         gets(keyInStr);
 
-        if (password_check2(keyInStr)) {
+        if (password_check2(keyInStr)) 
+		{
             printf("잘 하였습니다\n");
             break;
         }
