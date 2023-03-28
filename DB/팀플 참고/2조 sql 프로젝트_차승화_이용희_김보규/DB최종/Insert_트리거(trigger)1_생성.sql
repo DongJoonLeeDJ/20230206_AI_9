cede9f2f68dc5b6c9a2dd8039e35943a09b77bd5
Xenoint <ldjgood12@gmail.com>
2023-03-28 16:49:08 +0900

    정리

--- DB/팀플 참고/2조 sql 프로젝트_차승화_이용희_김보규/DB최종/Insert_트리거(trigger)1_생성.sql
+++
@@ -1,8 +0,0 @@
-create or replace trigger trig_room_add 
-after insert on treatchart
-for each row 
-when (new.prescription like '%ют©Ь%') 
-begin 
-    insert into room values(trig_room_num.nextval,:new.time, :new.doc_id, :new.nur_id, :new.pat_id, :new.diagnosis);
- end trig_room;
-
--- DB/팀플 참고/2조 sql 프로젝트_차승화_이용희_김보규/DB최종/Update_트리거(trigger)3_생성.sql
+++
@@ -1,9 +0,0 @@
-create or replace trigger trig_room_upd 
-after insert or update on treatchart
-for each row 
-when (new.prescription like '%수술%') 
-begin
-    update room set time = :new.time where pat_id = :new.pat_id;
-    update room set diagnosis = :new.diagnosis||' - 수술중' 
-           where pat_id = :new.pat_id;
- end trig_room_upd; 
