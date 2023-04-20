434bc859d251872877db66241b07d5a38870b620
Xenoint <ldjgood12@gmail.com>
2023-04-20 10:50:29 +0900

    js 두번째날 및 정처기 기출

---
+++ js/02_20230419/05_rcp.js
@@ -0,0 +1,4 @@
+//첫번째는 가위
+//두번째는 바위
+//세번재는 보
+let gbb = ['https://img.freepik.com/premium-vector/scissor-black-glyph-icon-flat-style-graphic-design-template-for-logo-app-ui-pictogram-silhouette-sim_222220-345.jpg','https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbHWdTV%2Fbtsar4ko1ZV%2FTqgb3iY5xZyM6za6ElfCH0%2Fimg.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQumDo6F0DhnSarbZN3Sr7XaQgBjQB_EgTK8Q&usqp=CAU']
